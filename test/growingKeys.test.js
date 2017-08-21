4a82705e0440b3394214c900ec6396253f428d61
Jon Lindell <jhlindell@yahoo.com>
2017-08-21 10:53:21 -0600

    changed equal to deepEqual and all tests pass

--- test/growingKeys.test.js
+++ test/growingKeys.test.js
@@ -4,11 +4,11 @@ const growingKeys = require('../growingKeys').growingKeys;
 
 describe("Growing Keys", function(){
   it('should return the proper number of keys in an object', function() {
-    assert.equal(growingKeys("test", 1), {test: true});
+    assert.deepEqual(growingKeys("test", 1), {test: true});
   });
 
   it('should return an empty object if the number passed in is zero', function(){
-    assert.equal(growingKeys("test", 0), {});
+    assert.deepEqual(growingKeys("test", 0), {});
   });
 
   it('should return an error if the key passed in is not a string', function(){
